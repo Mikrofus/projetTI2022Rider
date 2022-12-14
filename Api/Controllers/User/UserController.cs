7e274f00db833b069e961b95eeadc046707d5de8
Kyllian Alland <kyllian1709@gmail.com>
2022-12-14 18:06:27 +0100

    JWT P1.3

--- Api/Controllers/User/UserController.cs
+++ Api/Controllers/User/UserController.cs
@@ -1,23 +1,32 @@
 ﻿using Application.UseCases.User;
 using Application.UseCases.User.Dto;
 using Infrastructure.Ef.DbEntities;
+using Infrastructure.Ef.DTOs;
+using Infrastructure.Security;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Api.Controllers.User;
 
 [ApiController]
-[Route("api/v1/users")]
+[Route("api/v1/users[controller]")]
 public class UserController : ControllerBase
 {
     private readonly UseCaseCreateUser _useCaseCreateUser;
     private readonly UseCaseFetchUserById _useCaseFetchUserById;
     private readonly UseCaseFetchAllUsers _useCaseFetchAllUser;
-
-    public UserController(UseCaseCreateUser useCaseCreateUser, UseCaseFetchUserById useCaseFetchUserById, UseCaseFetchAllUsers useCaseFetchAllUser)
+    
+    //JWT
+    private readonly IConfiguration _config;
+    private readonly ITokenService _tokenService;
+    private string _generatedToken = null;  
+    public UserController(UseCaseCreateUser useCaseCreateUser, UseCaseFetchUserById useCaseFetchUserById, UseCaseFetchAllUsers useCaseFetchAllUser, ITokenService tokenService, IConfiguration config)
     {
         _useCaseCreateUser = useCaseCreateUser;
         _useCaseFetchUserById = useCaseFetchUserById;
         _useCaseFetchAllUser = useCaseFetchAllUser;
+        _tokenService = tokenService;
+        _config = config;
     }
 
     [HttpGet]
@@ -47,6 +56,7 @@ public class UserController : ControllerBase
     }
 
     [HttpPost]
+    [Route("create")]
     public ActionResult<DtoOutputUser> Create(DtoInputCreateUser dto)
     {
         var output = _useCaseCreateUser.Execute(dto);
@@ -56,4 +66,37 @@ public class UserController : ControllerBase
             output
         );
     }
+    
+    [HttpGet]
+    [Route("login")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    public ActionResult BuildToken(string pseudo, string password)
+    {
+        IEnumerable<DtoOutputUser> users = _useCaseFetchAllUser.Exectue();
+        DtoOutputUser u1 = users
+            .Where(u => u.Pseudo.ToUpper().Equals(pseudo.ToUpper()) && u.Pass.ToUpper().Equals(password.ToUpper()))
+            .FirstOrDefault();
+        if (u1 != null)
+        {
+            string key = _config["Jwt:Key"];
+            string token = _tokenService.BuildToken(key, "Oui", u1);
+            Response.Cookies.Append("Kyky le cookie", token);
+            return Ok("Yeaaaah ça marche !");
+        }
+
+        return Problem("PasOk");
+    }
+
+    
+    
+    [HttpPost]
+    [Route("login")]
+    [Authorize]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    public ActionResult ValidateToken(string pseudo, string password)
+    {
+        return Ok("T'es accepté héhé!");
+    }
+    
+
 }
